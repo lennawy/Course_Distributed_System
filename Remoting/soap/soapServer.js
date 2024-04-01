aea8973d1eb6c3f87dcfa1a5e69bbf61049b826a
Lena Wu <lennawy2020@gmail.com>
2024-04-01 13:47:01 +0800

    Time:240401

---
+++ Remoting/soap/soapServer.js
@@ -0,0 +1,24 @@
+const soap = require('soap');
+const http = require('http');
+const xml = require('fs').readFileSync('AddMul.wsdl', 'utf8');
+const service = {
+    CalculatorImplService: {
+        CalculatorImplPort: {
+            // add: function (args) {
+            //     return {result: args.x + args.y};
+            // },
+            multiply: function (args) {
+                return {result: args.x * args.y}; // 新增的 multiply 方法定義
+            }
+        }
+    }
+}
+const server = http.createServer(function (request, response) {
+    response.end('404: Not Found: ' + request.url);
+});
+
+soap.listen(server, '/AddMul', service, xml, function () {
+    console.log('server initialized');
+});
+
+server.listen(8192);
